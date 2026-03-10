6b40764a655df4d85b0bb74916382889a1ca498d
Kai Nell <kai@bluurock.com>
2026-03-10 10:01:15 +0200

    CA Disputes

--- src/components/admin/shift-management/AdminShifts.tsx
+++ src/components/admin/shift-management/AdminShifts.tsx
@@ -26,7 +26,7 @@ const AVATAR_COLORS = [
   '#E57373', '#F06292', '#BA68C8', '#7986CB', '#64B5F6',
   '#4DD0E1', '#4DB6AC', '#81C784', '#FFB74D', '#A1887F',
 ];
-
+// 
 function hashString(str: string): number {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
